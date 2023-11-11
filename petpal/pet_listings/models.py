6e5b8b6b9c95cb8170143e0c23b23964949158f6
Aviral Bhardwaj <aviral.bhardwaj@mail.utoronto.ca>
2023-11-10 19:11:48 -0500

    JWT & PetLIsting
    JWT and pet listings update

--- petpal/pet_listings/models.py
+++ petpal/pet_listings/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+# import 
 
 # Create your models here.
+class Pets(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    location = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    shelter = models.CharField(max_length=100)
+    status = models.CharField(max_length=100, default="Available")
+    date_added = models.DateTimeField(auto_now_add=True)
+    # Shelter accounts.User should be changed to accounts.Shelter when Shelter model is created
+    
+    # shelter = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='pets')
+    # application = models.ForeignKey('applications.Applications', on_delete=models.CASCADE, related_name='pets', null=True)
+    # comments = models.ForeignKey('comments.Comments', on_delete=models.CASCADE, related_name='pets', null=True)
+
+    def __str__(self):
+        return self.name
+    
+class Collections(models.Model):
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    pets = models.ManyToManyField('Pets', related_name='collections')
+  
--- petpal/pet_listings/urls.py
+++ petpal/pet_listings/urls.py
@@ -3,6 +3,17 @@ from . import views
 
 app_name="pet_listings"
 urlpatterns = [ 
+    # path('all/', views.all_pets, name="all_pets"),
+    
+    # path('search/', views.search, name="search"),
+    # path('add/', views.add_pet, name="add_pet"),
+    # path('edit/<int:pet_id>/', views.edit_pet, name="edit_pet"),
+    # path('delete/<int:pet_id>/', views.delete_pet, name="delete_pet"),
+    # path('search/<int:pet_id>/', views.pet_detail, name="pet_detail"),
+    # path('<int:pet_id>/like/', views.add_to_collection, name="add_to_collection"),
+    # path('<int:pet_id>/unlike/', views.remove_from_collection, name="remove_from_collection"),
+    # path('<int:pet_id>/adopt/', views.adopt, name="adopt"),
+
     # path('login/', views.LoginView.as_view(), name="login"),
     # path('logout/', views.logout, name="logout"),
 ]
