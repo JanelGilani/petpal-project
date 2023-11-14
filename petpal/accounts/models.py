52ff0a281140e30439dc4aa9a8402e4d49bd27eb
Janel Gilani <gilani.janel@gmail.com>
2023-11-14 12:21:40 -0500

    CustomUser update

--- petpal/accounts/models.py
+++ petpal/accounts/models.py
@@ -1,21 +1,27 @@
 # accounts/models.py
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    seeker = models.BooleanField(default=False)
+    shelter = models.BooleanField(default=False)
+
 
 class Shelter(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    contact_information = models.CharField(max_length=255)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
     mission_statement = models.TextField()
 
-    def delete(self, *args, **kwargs):
-        # Delete associated User instance
-        self.user.delete()
-        super().delete(*args, **kwargs)
+    # def delete(self, *args, **kwargs):
+    #     # Delete associated User instance
+    #     self.user.delete()
+    #     super().delete(*args, **kwargs)
 
 class PetSeeker(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    contact_information = models.CharField(max_length=255)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    preferences = models.TextField()
+    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True, null=True)
